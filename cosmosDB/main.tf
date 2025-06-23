a1ad64347b113484e8ce5dfc00056f92f6ed987b
neeraj pandey <np375431@gmail.com>
2025-06-23 22:24:36 +0530

    cosmos db

---
+++ cosmosDB/main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "cosmosdb-demo-rg"
+  location = "East US"
+}
+
+# Cosmos DB Account
+resource "azurerm_cosmosdb_account" "cosmos" {
+  name                = "cosmosdbdemoacct123"
+  location            =
